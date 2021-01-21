1e15357cd19ed4b69a67c57939a3a4dd131920db
Cosmsos <cosmosappiah029@gmail.com>
2021-01-21 22:44:45 +0000

    added comments

--- index.ts
+++ index.ts
@@ -3,10 +3,12 @@ import { QuestionCollection, Answers, prompt } from "inquirer";
 async function main() {
   console.log("Test scorer v.0.0.1");
 
+  // function to remove whitespaces from string
   const removeWhiteSpaces = (text: string): string => {
     return text.replace(/\s/g, "").trim();
   };
 
+  //an array of question to prompt the user
   const questions: QuestionCollection = [
     {
       name: "correct_answers",
@@ -34,9 +36,11 @@ async function main() {
 
   let studentAnswers: Array<any> = [];
 
+  //remove white spaces and use regex to capture groups of answers
   removeWhiteSpaces(answers.student_answers)
     .match(/([0-9]+)([tfxTFX]+)/g)!
     .forEach(answer => {
+      // turn answers into an object of ids and answers and push them into the answers array
       studentAnswers.push({ [answer.substring(0, 3)]: answer.substring(3) });
     });
 
@@ -44,6 +48,7 @@ async function main() {
     let score: number = 0;
 
     for (let id in studentAnswer) {
+      // loop true the correct answers, compare with the student answers and mark them
       for (let i: number = 0; i < correctAnswers.length; i++) {
         if (studentAnswer[id][i] !== correctAnswers[i]) {
           if (studentAnswer[id][i] == "x") score += 0;
